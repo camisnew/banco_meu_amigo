5d2936481a63aeea3271dc34871f810891d3355c
camisnew <camilabertholdpires@gmail.com>
2021-08-14 15:47:39 -0300

    remove debug flag

--- lib/view/my_app.dart
+++ lib/view/my_app.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Trabalho 1n2',
       theme: ThemeData(
         primarySwatch: Colors.red,
