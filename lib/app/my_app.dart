d3e6ee733021c3675a9649707047d4b181761cf7
camisnew <camilabertholdpires@gmail.com>
2021-08-15 23:19:04 -0300

    main color set

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Amigo - NoBanco',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: Colors.blueGrey,
       ),
     );
   }
